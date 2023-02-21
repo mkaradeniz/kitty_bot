6aa6e08d9e4a533941e2a4d041aa6bc6f39a2e68
Max Karadeniz <max@karadeniz.io>
2023-02-21 12:44:09 +0100

    fix: reminder

--- src/message/sendReminder.ts
+++ src/message/sendReminder.ts
@@ -4,6 +4,7 @@ import createSendEmailReminder from './sendEmailReminder';
 import createSendMessageWithoutContext from '../utils/message/createSendMessageWithoutContext';
 import envConfig from '../config/env';
 import getOrCreateCurrentQuizDb from '../db/getOrCreateCurrentQuiz';
+import getPlayersOutCount from '../utils/state/getPlayersOutCount';
 import getPlayersPlayingCount from '../utils/state/getPlayersPlayingCount';
 import isNotNullOrUndefined from '../utils/misc/isNotNullOrUndefined';
 import logger from '../utils/logger';
@@ -26,18 +27,18 @@ const createSendReminder = (bot: Telegraf<any>) => async () => {
     const currentQuiz = await getOrCreateCurrentQuizDb();
 
     const isEmailSent = currentQuiz.isEmailSent;
-    const playersOutCount = getPlayersPlayingCount(currentQuiz);
+    const playersOutCount = getPlayersOutCount(currentQuiz);
     const playersPlayingCount = getPlayersPlayingCount(currentQuiz);
 
     if (isEmailSent) {
-      logger.silly(`Reminder not sent because email is already sent.`, { label: 'src/message/sendReminder.ts:33' });
+      logger.silly(`Reminder not sent because email is already sent.`, { label: 'src/message/sendReminder.ts:34' });
 
       return;
     }
 
     if (playersPlayingCount >= envConfig.maxPlayers) {
       logger.silly(`Reminder not sent because players playing count is higher than max players.`, {
-        label: 'src/message/sendReminder.ts:40',
+        label: 'src/message/sendReminder.ts:41',
       });
 
       return;
@@ -46,7 +47,7 @@ const createSendReminder = (bot: Telegraf<any>) => async () => {
     if (playersPlayingCount === envConfig.maxPlayers) {
       await createSendEmailReminder(bot)();
 
-      logger.silly(`Email reminder sent instead of reminder, because of completed lineup.`, { label: 'src/message/sendReminder.ts:49' });
+      logger.silly(`Email reminder sent instead of reminder, because of completed lineup.`, { label: 'src/message/sendReminder.ts:50' });
 
       return;
     }
@@ -67,7 +68,7 @@ const createSendReminder = (bot: Telegraf<any>) => async () => {
       },
     );
 
-    logger.silly(`Reminder sent.`, { label: 'src/message/sendReminder.ts:70' });
+    logger.silly(`Reminder sent.`, { label: 'src/message/sendReminder.ts:71' });
   } catch (err) {
     logger.error(err);
   }
