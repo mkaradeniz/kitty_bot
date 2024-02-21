eb4e1f7f3b8a704718921077f2691df2acdadc69
Max Karadeniz <max@karadeniz.io>
2024-02-21 18:20:07 +0100

    chore: remove timestamps from production console transport log

--- src/utils/logger/transports/consoleTransport.ts
+++ src/utils/logger/transports/consoleTransport.ts
@@ -9,6 +9,16 @@ import { stringify } from '@utils/misc/stringify';
 const { colorize, combine, printf, timestamp } = winstonFormat;
 
 const customFormatter = printf(info => {
+  if (!isNotNullOrUndefined(info.timestamp)) {
+    if (typeof info.message === 'object') {
+      info.message = stringify(info.message, null, 2);
+    }
+
+    const label = isNotNullOrUndefined(info.label) ? `[\x1b[2m${info.label}\x1b[0m]` : undefined;
+
+    return [`${info.level}`, label, `${info.message}`].filter(isNotNullOrUndefined).join(' ');
+  }
+
   const formattedDate = formatDate(new Date(info.timestamp as string), 'yyyy-MM-dd HH:mm:ss:SSS');
 
   if (typeof info.message === 'object') {
